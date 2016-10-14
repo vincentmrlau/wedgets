27c46352beba6e6d2c857967afe221a08360006e
vincent_lau <413893093@qq.com>
2016-10-14 11:51:00 +0800

    debug

--- qiniu/vincent-qiniu.js
+++ qiniu/vincent-qiniu.js
@@ -47,128 +47,131 @@
 vin.qiniuInitialize = function(options)
 {
 	if (!(typeof options == "object")) {
-		console.error("qiniuInitialize:options is undefined");
-		return false;
-	}
-	
-	// set form
-	var formDom = document.createElement("form");
-	var inputDom = document.createElement("input");
-	inputDom.setAttribute("type","file");
-	inputDom.setAttribute("accept"
-		,options.accept?options.accept:"image/*");
-	var multipleAtt = document.createAttribute("multiple");
-	if (options.multiple === false) {
-	}
-	else{
-		inputDom.setAttributeNode(multipleAtt);
-	} 
-	formDom.appendChild(inputDom);
-	document.body.appendChild(formDom);
-	formDom.style.zIndex = 100000;
-
-	//set button
-	try{
-		//compute button style
-		var setWidth = 0;
-		var setHight = 0;
+			console.error("qiniuInitialize:options is undefined");
+			return false;
+		}
 		
-		//if display = none, show it first
-		if(window.getComputedStyle(options.button).display =="none"){
-			// console.log("none");
-			var isNone = true;
-			options.button.style.display = "inline-block";
+		// set form
+		var formDom = document.createElement("form");
+		var inputDom = document.createElement("input");
+		inputDom.setAttribute("type","file");
+		inputDom.setAttribute("accept"
+			,options.accept?options.accept:"image/*");
+		var multipleAtt = document.createAttribute("multiple");
+		if (options.multiple === false) {
 		}
-		var buttonStyle = window.getComputedStyle(options.button);
-		var buttonWidth = parseFloat(buttonStyle.width);
-		var buttonHeight = parseFloat(buttonStyle.height);
-		var buttonPaddingLeft = parseFloat(buttonStyle.paddingLeft);
-		var buttonPaddingRight = parseFloat(buttonStyle.paddingRight);
-		var buttonPaddingTop = parseFloat(buttonStyle.paddingTop);
-		var buttonPaddingBottom = parseFloat(buttonStyle.paddingBottom);
-		var buttonBorderLeft = parseFloat(buttonStyle.borderLeft);
-		var buttonBorderRight = parseFloat(buttonStyle.borderRight);
-		var buttonBorderTop = parseFloat(buttonStyle.borderTop);
-		var buttonBorderBottom = parseFloat(buttonStyle.borderBottom);
-		setWidth = buttonWidth;
-		setHight = buttonHeight;
-		console.log($(options.button).css("width"));					
-		options.button.appendChild(formDom);
-		formDom.style.position = "relative";					
-		formDom.style.display = "inline-block";
-		formDom.style.top = (0-buttonPaddingTop-buttonBorderTop+2)+"px";
-		formDom.style.left = (0-buttonPaddingLeft-buttonBorderLeft+2)+"px";
-		formDom.style.float = "left";
-		formDom.style.width = "0px";
-		formDom.style.height = "0px";
-		formDom.style.margin = "0px";
-		formDom.style.padding = "0px";
-		formDom.style.border = "0px";
-		formDom.style.zIndex = "10000";
-		inputDom.style.border = "0px";
-		inputDom.style.margin = "0px";
-		inputDom.style.padding = "0px";
-		inputDom.style.width = (setWidth-5)+"px";
-		inputDom.style.height = (setHight-5)+"px";
-		inputDom.style.position = "relative";
-		inputDom.style.overflow = "hidden";
-		formDom.style.zoom = "1";
-		inputDom.style.opacity = "0";
-		inputDom.style.filter = 'alpha(opacity=0)';
-
-		//set ori-display
-		if(isNone){
-			options.button.style.display = "none";
-		}
-	}catch(e){throw(e);}
-	var bucket = qiniu.bucket(
-		options.title?options.title:"vincent-qiniu"
-		,{
-			putToken:options.uptoken
-			,url:options.domains
-		});
-	inputDom.onchange = function(){
-		var files = inputDom.files;
-		var uploadstuts = files.length;//when success 0
-		for(var i =0;i<files.length;i++){
-
-			var name =null;
-			if(typeof options.before == "function"){
-				var beforeReturn = options.before(name,files[i]);
-				if(typeof beforeReturn =="object"){
-					name = beforeReturn.name
-							?beforeReturn.name
-							:name;
-					files[i] = beforeReturn.file
-							?beforeReturn.file
-							:files[i];
-				}				
+		else{
+			inputDom.setAttributeNode(multipleAtt);
+		} 
+		
+		//set button
+		try{
+			//compute button style
+			var setWidth = 0;
+			var setHight = 0;
+			
+			//if display = none, show it first
+			if(window.getComputedStyle(options.button).display =="none"){
+				// console.log("none");
+				var isNone = true;
+				options.button.style.display = "inline-block";
 			}
-			//upload files
-			bucket.putFile(name,files[i])
-			.then(
-				function(reply){
-				    // success
-				    if (typeof options.success == "function") {
-				    	options.success(reply);
-				    }
-				    uploadstuts -= 1;
-				    if (uploadstuts == 0) {
-				    	formDom.reset();
-				    }  
+			var buttonStyle = window.getComputedStyle(options.button);
+			var buttonWidth = parseFloat(buttonStyle.width);
+			var buttonHeight = parseFloat(buttonStyle.height);
+			var buttonPaddingLeft = parseFloat(buttonStyle.paddingLeft);
+			var buttonPaddingRight = parseFloat(buttonStyle.paddingRight);
+			var buttonPaddingTop = parseFloat(buttonStyle.paddingTop);
+			var buttonPaddingBottom = parseFloat(buttonStyle.paddingBottom);
+			var buttonBorderLeft = parseFloat(buttonStyle.borderLeft);
+			var buttonBorderRight = parseFloat(buttonStyle.borderRight);
+			var buttonBorderTop = parseFloat(buttonStyle.borderTop);
+			var buttonBorderBottom = parseFloat(buttonStyle.borderBottom);
+			setWidth = buttonWidth;
+			setHight = buttonHeight;
+			console.log($(options.button).css("width"));					
+			formDom.style.position = "relative";					
+			formDom.style.display = "inline-block";
+			formDom.style.top = (0-buttonPaddingTop-buttonBorderTop+2)+"px";
+			formDom.style.left = (0-buttonPaddingLeft-buttonBorderLeft+2)+"px";
+			formDom.style.float = "left";
+			formDom.style.width = "0px";
+			formDom.style.height = "0px";
+			formDom.style.margin = "0px";
+			formDom.style.padding = "0px";
+			formDom.style.border = "0px";
+			formDom.style.zIndex = "10000";
+			inputDom.style.border = "0px";
+			inputDom.style.margin = "0px";
+			inputDom.style.padding = "0px";
+			inputDom.style.width = (setWidth-5)+"px";
+			inputDom.style.height = (setHight-5)+"px";
+			inputDom.style.position = "relative";
+			inputDom.style.overflow = "hidden";
+			formDom.style.zoom = "1";
+			inputDom.style.opacity = "0";
+			inputDom.style.filter = 'alpha(opacity=0)';
+			formDom.style.zIndex = 100000;
+
+			formDom.appendChild(inputDom);
+			if (options.button.firstChild){
+				options.button.insertBefore(formDom,options.button.firstChild);
+			}else{
+				options.button.appendChild(formDom);
+			}						
+			//set ori-display
+			if(isNone){
+				options.button.style.display = "none";
+			}
+		}catch(e){throw(e);}
+		var bucket = qiniu.bucket(
+			options.title?options.title:"vincent-qiniu"
+			,{
+				putToken:options.uptoken
+				,url:options.domains
+			});
+		inputDom.onchange = function(){
+			var files = inputDom.files;
+			var uploadstuts = files.length;//when success 0
+			for(var i =0;i<files.length;i++){
+
+				var name =null;
+				if(typeof options.before == "function"){
+					var beforeReturn = options.before(name,files[i]);
+					if(typeof beforeReturn =="object"){
+						name = beforeReturn.name
+								?beforeReturn.name
+								:name;
+						files[i] = beforeReturn.file
+								?beforeReturn.file
+								:files[i];
+					}				
 				}
-				,function(err){
-					if (typeof options.error == "function") {
-						options.error(err);
+				//upload files
+				bucket.putFile(name,files[i])
+				.then(
+					function(reply){
+					    // success
+					    if (typeof options.success == "function") {
+					    	options.success(reply);
+					    }
+					    uploadstuts -= 1;
+					    if (uploadstuts == 0) {
+					    	formDom.reset();
+					    }  
 					}
-					uploadstuts -= 1;
-					if (uploadstuts == 0) {
-						formDom.reset();
-					}      
-				}
-				);
+					,function(err){
+						if (typeof options.error == "function") {
+							options.error(err);
+						}
+						uploadstuts -= 1;
+						if (uploadstuts == 0) {
+							formDom.reset();
+						}      
+					}
+					);
+			}
 		}
-	}
 }
 
 })(vin)
